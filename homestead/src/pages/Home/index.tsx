a1dc9b1e773526a9fb4f73385965734237cd0b5a
AustinMerilos <67397590+AustinMerilos@users.noreply.github.com>
2022-09-27 15:30:38 -0700

    feat: adding style components

--- homestead/src/pages/Home/index.tsx
+++ homestead/src/pages/Home/index.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { BuyContainer, HeaderContainer, RentContainer } from "./styles";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <HeaderContainer> Homestead</HeaderContainer>
+      <RentContainer> Rent homes here</RentContainer>
+      <BuyContainer>Buy a home here</BuyContainer>
+    </div>
+  );
 }
---
+++ homestead/src/pages/Home/styles.ts
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.div`
+  background: linear-gradient(
+      90deg,
+      #050d1eb2 0%,
+      #050d1e29 40%,
+      transparent 50%
+    ),
+  height: 46vw;
+  width: 100vw;
+`;
+export const RentContainer = styled.div``;
+export const RentButton = styled.button``;
+export const BuyContainer = styled.div``;
+export const BuyButton = styled.button``;
