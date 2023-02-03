1c878d5666621e48708927146f97c62c842877e4
AustinMerilos <67397590+AustinMerilos@users.noreply.github.com>
2023-02-03 15:41:22 -0800

    feat: search page

---
+++ homestead/src/pages/Search/styles.ts
@@ -0,0 +1,58 @@
+import { Button, Grid, Typography } from "@material-ui/core";
+import styled from "styled-components";
+
+
+export const Container = styled(Grid).attrs({
+  container: true,
+  direction: 'row',
+})`
+  display: flex;
+  background-color: lightgrey;
+  padding: 60px 120px 10px;
+  margin-top: 2vw;
+
+`
+export const InnerContainer = styled(Grid).attrs({
+  container: true,
+  direction: 'row',
+})`
+  display: flex;
+  padding: 60px 120px 10px;
+  margin-top: 4vw;
+
+`
+export const NoResultsContainer = styled(Grid).attrs({
+  container: true,
+  direction: 'column',
+})`
+  display: flex;
+  align-content: center;
+
+`
+export const Title= styled.div`
+
+`
+export const NoResultsTitle= styled(Typography).attrs({
+  variant: 'h2',
+})
+`
+  align-self: center;
+  font-weight: bold;
+
+`
+export const FilterButton= styled(Button)`
+    && {
+    margin-left: inherit;
+    margin-bottom: auto
+  }
+    
+`
+
+export const NoResults= styled.img`
+
+  display: flex;
+  width: 40%;
+   
+
+`
+;
